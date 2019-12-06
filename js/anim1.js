235278aa0b30faf8b19262d9685ad918f6d61d0e
thomas <thomas.deasington@etu.uca.fr>
2019-12-06 12:04:21 +0100

    new button to start the animation / animation responsive

--- js/anim1.js
+++ js/anim1.js
@@ -1,5 +1,17 @@
 var rain = [];
 var floor;
+var started;
+var buttonPlay;
+
+window.onload = function(){
+    this.buttonPlay();
+}
+
+function buttonPlay(){
+    buttonPlay = createButton("Play");
+    buttonPlay.position(windowWidth/2.17,windowHeight/2.1);
+    buttonPlay.mousePressed(start);
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight*0.99);
@@ -8,11 +20,11 @@ function setup() {
     floor.clear();
 
     for(i = 0; i < 150; i++){
-        rain.push(new Drop(random(0,2*windowWidth), random(-300,-150), random(2.5,50), random(1,windowHeight*0.01)));
+        rain.push(new Drop(random(0,2*windowWidth), random(-300,-150), random(7,23), random(1,windowHeight*0.01)));
     }
     
     for(i = 0; i < 150; i++){
-        rain.push(new Drop(random(0,2*windowWidth), random(-150,0), random(2.5,50), random(1,windowHeight*0.01)));
+        rain.push(new Drop(random(0,2*windowWidth), random(-150,0), random(7,23), random(1,windowHeight*0.01)));
     }
 
     background(0);
@@ -21,28 +33,41 @@ function setup() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
     floor = createGraphics(windowWidth, windowHeight*0.99);
-    document.getElementById("container").margin = "0";
-    document.getElementById("container").width= "100%";
+    buttonPlay.position(windowWidth/2.17,windowHeight/2);
+    document.documentElement.style.overflow = 'hidden';
+    document.body.scroll = "no";
 }
 window.addEventListener('resize', windowResized);
 
 
 function draw() {
+    if(started){
+        clear();
+        smooth();
+        noFill();
 
-    clear();
-    smooth();
-    noFill();
+        floor.stroke(255);
+        floor.line(0, windowHeight/1.5, windowWidth, windowHeight/1.5);
+        rain.forEach(function(drop){
+            drop.show();
+            drop.fall();
+            if(drop.getY() > windowHeight/1.515){
+                drop.setX(random(0,2*windowWidth));
+                drop.setY(random(-100,0));
+            }
+        });
+        
+        image(floor,0,0);
+    }
+}
 
-    floor.stroke(255);
-    floor.line(0, windowHeight/1.5, windowWidth, windowHeight/1.5);
-    rain.forEach(function(drop){
-        drop.show();
-        drop.fall();
-        if(drop.getY() > windowHeight/1.515){
-            drop.setX(random(0,2*windowWidth));
-            drop.setY(random(-100,0));
-        }
-    });
-    
-    image(floor,0,0);
+function start(){
+    started = true;
+    loop();
+
+    var audio = new Audio('ext/rain_sound_effect.wav');
+    audio.play();
+    audio.volume = 0.6;
+
+    buttonPlay.remove();
 }
